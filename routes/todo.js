b085070f8f7b72247e906cf583ae46f604037ce8
Sudip Sarkar <sudipsarkar419@gmail.com>
2023-04-20 13:29:38 +0530

    implement jwt/token

--- routes/todo.js
+++ routes/todo.js
@@ -2,9 +2,17 @@ const express = require('express');
 const router=express.Router();
 
 const {createTask,updateTask,deleteTask,getTask} =require('../controllers/todoController');
+const auth = require('../config/middleware');
 
-router.route('/').post(createTask);
-router.route('/:id').put(updateTask);
-router.route('/:id').delete(deleteTask);
-router.route('/:id').get(getTask);
+// router.route('/').post(createTask);
+// router.route('/:id').put(updateTask);
+// router.route('/:id').delete(deleteTask);
+// router.route('/:id').get(getTask);
+
+    router.post('/create-task',auth,createTask);
+    router.put('/:id',auth,updateTask);
+    router.delete('/:id',auth,deleteTask);
+    router.get('/:id',auth,getTask);
+
+
 module.exports = router;
